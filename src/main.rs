734f8868807bce786f5bd75d80665a3f8f105660
Hanno Braun <hb@hannobraun.de>
2019-08-02 19:31:43 +0200

    Stream source code instead of reading completely

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 mod interpreter;
+mod reader;
 mod tokenizer;
 
 
 use std::{
     env,
     fs::File,
-    io::prelude::*,
 };
 
 
 fn main() {
     let program = env::args().skip(1).next().unwrap();
     let file    = format!("examples/{}.kr", program);
+    let program = File::open(file).unwrap();
 
-    let mut program = String::new();
-    File::open(file)
-        .unwrap()
-        .read_to_string(&mut program)
-        .unwrap();
+    let mut reader      = reader::Reader::new(program);
+    let     tokenizer   = tokenizer::Tokenizer::new(&mut reader);
+    let     interpreter = interpreter::Interpreter::new(tokenizer);
 
-    let tokenizer   = tokenizer::Tokenizer::new(program.chars());
-    let interpreter = interpreter::Interpreter::new(tokenizer);
+    let result = interpreter.run();
 
-    if let Err(error) = interpreter.run() {
+    if let Some(error) = reader.error() {
+        print!("{:?}", error);
+    }
+    if let Err(error) = result {
         print!("{}", error);
     }
 }
---
+++ src/reader.rs
@@ -0,0 +1,96 @@
+use std::{
+    io::{
+        self,
+        prelude::*,
+    },
+    str::{
+        self,
+        Utf8Error,
+    },
+};
+
+
+pub struct Reader<R> {
+    reader: R,
+    buffer: [u8; 4],
+    index:  usize,
+    error:  Option<Error>,
+}
+
+impl<R> Reader<R> {
+    pub fn new(reader: R) -> Self {
+        Reader {
+            reader,
+            buffer: [0; 4],
+            index:  0,
+            error:  None,
+        }
+    }
+
+    pub fn error(self) -> Option<Error> {
+        self.error
+    }
+}
+
+impl<'r, R> Iterator for &'r mut Reader<R> where R: Read {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let c = loop {
+            let result = self.reader.read_exact(
+                &mut self.buffer[self.index ..= self.index]
+            );
+            self.index += 1;
+
+            match result {
+                Ok(()) => (),
+                Err(error) => {
+                    match error.kind() {
+                        io::ErrorKind::UnexpectedEof => {
+                            ()
+                        }
+                        _ => {
+                            self.error = Some(Error::Io(error));
+                        }
+                    }
+
+                    return None;
+                }
+            }
+
+            match str::from_utf8(&self.buffer[.. self.index]) {
+                Ok(s) => {
+                    assert_eq!(s.chars().count(), 1);
+
+                    // Can't panic. We just asserted that there is exactly one
+                    // char.
+                    break s.chars().next().unwrap();
+                }
+                Err(error) => {
+                    match self.index {
+                        i if i == 4 => {
+                            self.error = Some(Error::Utf8(error));
+                            return None;
+                        }
+                        i if i > 4 => {
+                            unreachable!();
+                        }
+                        _ => {
+                            continue;
+                        }
+                    }
+                }
+            }
+        };
+
+        self.index = 0;
+        Some(c)
+    }
+}
+
+
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    Utf8(Utf8Error),
+}
