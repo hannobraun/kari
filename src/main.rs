2e75f64038bae349870a304ec4118e70200fd279
Hanno Braun <hb@hannobraun.de>
2019-08-02 19:45:37 +0200

    Add support for entering program via stdin

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,29 @@ mod tokenizer;
 use std::{
     env,
     fs::File,
+    io,
 };
 
 
 fn main() {
-    let program = env::args().skip(1).next().unwrap();
-    let file    = format!("examples/{}.kr", program);
-    let program = File::open(file).unwrap();
+    let result = match env::args().count() {
+        1 => {
+            runner::run(io::stdin().lock())
+        },
+        2 => {
+            let program = env::args().skip(1).next().unwrap();
+            let file    = format!("examples/{}.kr", program);
+            let program = File::open(file).unwrap();
 
-    if let Err(error) = runner::run(program) {
+            runner::run(program)
+        }
+        _ => {
+            print!("ERROR: Expecting zero or one arguments");
+            return;
+        }
+    };
+
+    if let Err(error) = result {
         match error {
             runner::Error::Reader(error)      => print!("{:?}", error),
             runner::Error::Interpreter(error) => print!("{}", error),
