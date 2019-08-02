c6f8592c35ef2a81139ccc9af913328d9d4cc43e
Hanno Braun <hb@hannobraun.de>
2019-08-02 17:09:30 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@ mod interpreter;
 mod tokenizer;
 
 
-use tokenizer::Tokenizer;
-
-
 fn main() {
     let program = include_str!("../examples/hello_world.kr");
 
-    let tokenizer   = Tokenizer::new(program.chars());
+    let tokenizer   = tokenizer::Tokenizer::new(program.chars());
     let interpreter = interpreter::Interpreter::new(tokenizer);
 
     interpreter.run();
