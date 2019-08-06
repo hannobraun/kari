aa264f0817877fdd577c969e1fe81a5520df385e
Hanno Braun <hb@hannobraun.de>
2019-08-06 17:26:13 +0200

    Implement `Stream` for mutable refs to `Stream`

--- src/stream.rs
+++ src/stream.rs
@@ -4,3 +4,12 @@ pub trait Stream {
 
     fn next(&mut self) -> Result<Self::Item, Self::Error>;
 }
+
+impl<T> Stream for &'_ mut T where T: Stream {
+    type Item  = T::Item;
+    type Error = T::Error;
+
+    fn next(&mut self) -> Result<Self::Item, Self::Error> {
+        <T as Stream>::next(self)
+    }
+}
