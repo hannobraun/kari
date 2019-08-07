a0fb01f42b9b03dcac3f69cf0b691c9b3e6542c3
Hanno Braun <hb@hannobraun.de>
2019-08-07 19:29:17 +0200

    Refactor

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,15 +1,21 @@
 use std::io;
 
-use crate::pipeline::{
-    evaluator::{
-        Error,
-        Evaluator,
+use crate::{
+    core::span::Span,
+    pipeline::{
+        evaluator::{
+            Error,
+            Evaluator,
+        },
+        parser::Parser,
+        reader::{
+            Char,
+            Reader,
+        },
+        recorder::Recorder,
+        stream::Stream,
+        tokenizer::Tokenizer,
     },
-    parser::Parser,
-    reader::Reader,
-    recorder::Recorder,
-    stream::Stream,
-    tokenizer::Tokenizer,
 };
 
 
@@ -48,65 +54,71 @@ fn print_error<Program>(
     print!("\nERROR: {}\n", error);
 
     if let Some(span) = error.span() {
-        let mut chars = recorder.chars().clone();
-        chars.retain(|c|
-
-            c.pos.line >= span.start.line
-                && c.pos.line <= span.end.line
-        );
-
-        print!(
-            "  => {}:{}:{}\n",
+        print_span(
+            span,
             program,
-            span.start.line + 1,
-            span.start.column + 1,
+            &mut recorder.chars().clone(),
         );
-        print!("      |\n");
-
-        // This makes heavy assumptions about the structure of `chars`,
-        // namely that chars' position's are consecutive, that chars in the
-        // same line actually have the same line recorded in their position,
-        // stuff like that.
-        for line in chars.split(|c| c.c == '\n') {
-            let first = match line.first() {
-                Some(first) => first.pos,
-                None        => continue,
-            };
-            let last = match line.last() {
-                Some(last) => last.pos,
-                None       => continue,
-            };
-
-            let start_column = if first.line == span.start.line {
-                span.start.column
-            }
-            else {
-                0
-            };
-            let end_column = if first.line == span.end.line {
-                span.end.column
-            }
-            else {
-                last.column
-            };
-
-            print!("{:5} | ", first.line + 1);
-            for c in line {
-                print!("{}", c.c);
-            }
-            print!("\n");
-
-            print!("      | ");
-            for column in 0 ..= last.column {
-                if column >= start_column && column <= end_column {
-                    print!("^");
-                }
-                else {
-                    print!(" ");
-                }
-            }
-        }
     }
 
     print!("\n");
+}
+
+fn print_span(span: Span, program: &str, chars: &mut Vec<Char>) {
+    chars.retain(|c|
+        c.pos.line >= span.start.line
+            && c.pos.line <= span.end.line
+    );
+
+    print!(
+        "  => {}:{}:{}\n",
+        program,
+        span.start.line + 1,
+        span.start.column + 1,
+    );
+    print!("      |\n");
+
+    // This makes heavy assumptions about the structure of `chars`,
+    // namely that chars' position's are consecutive, that chars in the
+    // same line actually have the same line recorded in their position,
+    // stuff like that.
+    for line in chars.split(|c| c.c == '\n') {
+        let first = match line.first() {
+            Some(first) => first.pos,
+            None        => continue,
+        };
+        let last = match line.last() {
+            Some(last) => last.pos,
+            None       => continue,
+        };
+
+        let start_column = if first.line == span.start.line {
+            span.start.column
+        }
+        else {
+            0
+        };
+        let end_column = if first.line == span.end.line {
+            span.end.column
+        }
+        else {
+            last.column
+        };
+
+        print!("{:5} | ", first.line + 1);
+        for c in line {
+            print!("{}", c.c);
+        }
+        print!("\n");
+
+        print!("      | ");
+        for column in 0 ..= last.column {
+            if column >= start_column && column <= end_column {
+                print!("^");
+            }
+            else {
+                print!(" ");
+            }
+        }
+    }
 }
