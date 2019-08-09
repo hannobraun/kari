7f2195b7b8016c4f0c449788d12dca034b2c633c
Hanno Braun <hb@hannobraun.de>
2019-08-09 20:34:43 +0200

    Refactor

--- src/pipeline/parser.rs
+++ src/pipeline/parser.rs
@@ -78,7 +78,8 @@ impl<Tokenizer> Parser<Tokenizer>
 
             let kind = match token.kind {
                 TokenKind::ListOpen => {
-                    expr::Kind::List(self.parse_list()?)
+                    let list = self.parse_list()?;
+                    expr::Kind::List(list)
                 }
                 TokenKind::ListClose => {
                     return Ok(expr::List(expressions));
