c4d43e71acf9dbfa3578bb5b174adaed5d757b3a
Hanno Braun <hb@hannobraun.de>
2019-08-10 10:45:42 +0200

    Move test runner into separate binary

--- src/bin/interpreter.rs
+++ src/bin/interpreter.rs
@@ -10,7 +10,6 @@ use clap::{
     App,
     Arg,
 };
-use walkdir::WalkDir;
 
 use kari::interpreter::evaluator::Evaluator;
 
@@ -26,80 +25,18 @@ fn main() {
                 .index(1)
                 .help("The program to execute, without the \".kr\" extension.")
         )
-        .arg(
-            Arg::with_name("test")
-                .long("test")
-                .short("t")
-                .help("Run a test if PATH is specified, all of them otherwise")
-        )
         .get_matches();
-
-    let kind = if args.is_present("test") {
-        ProgramKind::Test
-    } else {
-        ProgramKind::Regular
-    };
 
     match args.value_of("path") {
         Some(name) => {
-            let path = format!("kr/{}/{}.kr", kind.base(), name);
+            let path = format!("kr/examples/{}.kr", name);
             run_program(path.into());
         }
         None => {
-            match kind {
-                ProgramKind::Regular => {
-                    Evaluator::run(
-                        "<stdin>".into(),
-                        Box::new(AccReader::new(stdin())),
-                    );
-                }
-                ProgramKind::Test => {
-                    print!("\n");
-
-                    for result in WalkDir::new("kr/tests") {
-                        let entry = match result {
-                            Ok(entry) => {
-                                entry
-                            }
-                            Err(error) => {
-                                print!(
-                                    "ERROR: Error walking tests directory: {}",
-                                    error,
-                                );
-                                if let Some(path) = error.path() {
-                                    print!(" ({})\n", path.display());
-                                }
-                                exit(1);
-                            }
-                        };
-
-                        let path = entry.path();
-                        let path = match path.to_str() {
-                            Some(path) => {
-                                path
-                            }
-                            None => {
-                                print!(
-                                    "ERROR: Cannot conver path to UTF-8: {}\n",
-                                    path.to_string_lossy(),
-                                );
-                                exit(1);
-                            }
-                        };
-
-                        if !path.ends_with(".kr") {
-                            continue;
-                        }
-
-                        let success = run_program(path.into());
-                        if success {
-                            print!("    OK {}\n", path);
-                        }
-                    }
-
-                    print!("\n");
-                }
-            }
+            Evaluator::run(
+                "<stdin>".into(),
+                Box::new(AccReader::new(stdin())),
+            );
         }
     }
 }
@@ -121,19 +58,4 @@ fn run_program(path: Cow<str>) -> bool {
     };
 
     Evaluator::run(path, Box::new(file))
-}
-
-
-enum ProgramKind {
-    Regular,
-    Test,
-}
-
-impl ProgramKind {
-    fn base(&self) -> &'static str {
-        match self {
-            ProgramKind::Regular => "examples",
-            ProgramKind::Test    => "tests",
-        }
-    }
 }
---
+++ src/bin/tester.rs
@@ -0,0 +1,67 @@
+use std::{
+    borrow::Cow,
+    fs::File,
+    process::exit,
+};
+
+use walkdir::WalkDir;
+
+use kari::interpreter::evaluator::Evaluator;
+
+
+fn main() {
+    print!("\n");
+
+    for result in WalkDir::new("kr/tests") {
+        let entry = match result {
+            Ok(entry) => {
+                entry
+            }
+            Err(error) => {
+                print!(
+                    "ERROR: Error walking tests directory: {}",
+                    error,
+                );
+                if let Some(path) = error.path() {
+                    print!(" ({})\n", path.display());
+                }
+                exit(1);
+            }
+        };
+
+        let path = entry.path();
+        let path = match path.to_str() {
+            Some(path) => {
+                path
+            }
+            None => {
+                print!(
+                    "ERROR: Cannot conver path to UTF-8: {}\n",
+                    path.to_string_lossy(),
+                );
+                exit(1);
+            }
+        };
+
+        if !path.ends_with(".kr") {
+            continue;
+        }
+
+        let file = File::open(path)
+            .unwrap_or_else(|error| {
+                print!(
+                    "\nERROR: Failed to open file {} ({})\n\n",
+                    path,
+                    error,
+                );
+                exit(1);
+            });
+
+        let success = Evaluator::run(Cow::Borrowed(path), Box::new(file));
+        if success {
+            print!("    OK {}\n", path);
+        }
+    }
+
+    print!("\n");
+}
