6ae54a0b1b3e3d3f5846300979ad697e94a0b419
Hanno Braun <hb@hannobraun.de>
2019-08-21 17:14:04 +0200

    Refactor

--- src/extensions.rs
+++ src/extensions.rs
@@ -17,19 +17,15 @@ pub type Extension<Host> =
     fn(Rc<RefCell<Host>>, &mut dyn Context, Span) -> Result<(), context::Error>;
 
 
-pub struct Extensions<Host> {
-    map: HashMap<String, Extension<Host>>,
-}
+pub struct Extensions<Host>(HashMap<String, Extension<Host>>);
 
 impl<Host> Extensions<Host> {
     pub fn new() -> Self {
-        Self {
-            map: HashMap::new(),
-        }
+        Self(HashMap::new())
     }
 
     pub fn get(&self, name: &str) -> Option<Extension<Host>> {
-        self.map
+        self.0
             .get(name)
             .map(|extension| *extension)
     }
