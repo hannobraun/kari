fe3f7da75d8e49633ded90695731232ea4d65e29
Hanno Braun <hb@hannobraun.de>
2019-09-01 07:46:08 +0200

    Fix function overriding

--- src/functions.rs
+++ src/functions.rs
@@ -2,10 +2,6 @@ use std::{
     cell::RefCell,
     collections::HashMap,
     fmt,
-    hash::{
-        Hash,
-        Hasher,
-    },
     rc::Rc,
 };
 
@@ -24,94 +20,124 @@ use crate::{
     },
 };
 
+
+#[derive(Debug)]
 pub struct Functions<T> {
-    functions:  HashMap<Signature, T>,
-    signatures: HashMap<String, usize>,
+    functions: HashMap<String, Node<T>>,
 }
 
-impl<T> Functions<T> where T: Clone {
+impl<T> Functions<T>
+    where T: Clone
+{
     pub fn new() -> Self {
         Self {
-            functions:  HashMap::new(),
-            signatures: HashMap::new(),
+            functions: HashMap::new(),
         }
     }
 
-    pub fn define(&mut self,
-        name:     String,
-        args:     &[&'static dyn Type],
-        function: T,
+    pub fn define<S>(&mut self,
+        name: S,
+        args: &[&'static dyn Type],
+        f:    T,
     )
         -> Result<&mut Self, Error>
+        where S: Into<String>
     {
-        self.functions.insert(
-            Signature {
-                name: name.clone(),
-                args: args.to_vec(),
-            },
-            function,
-        );
+        let name = name.into();
 
-        self.signatures
+        if args.len() == 0 {
+            if self.functions.contains_key(&name) {
+                return Err(Error::FunctionAlreadyDefined);
+            }
+
+            self.functions.insert(
+                name,
+                Node::Function(f),
+            );
+            return Ok(self);
+        }
+
+        let node = self.functions
             .entry(name)
-            .and_modify(|num| *num = usize::max(*num, args.len()))
-            .or_insert(args.len());
+            .or_insert(Node::Type(HashMap::new()));
+
+        node.insert(args, f)?;
 
         Ok(self)
     }
 
     pub fn get(&self, name: &str, stack: &Stack) -> Option<T> {
-        for n in 0 ..= self.signatures.get(name).map(|n| *n).unwrap_or(0) {
-            let mut args: Vec<&'static dyn Type> = stack
-                .peek()
-                .take(n)
-                .map(|expr| expr.get_type())
-                .collect();
-            args.reverse();
+        let mut node = self.functions.get(name)?;
 
-            let function = self.functions
-                .get(&Signature { name: String::from(name), args })
-                .map(|function| function.clone());
+        for expr in stack.peek() {
+            let map = match node {
+                Node::Type(map)   => map,
+                Node::Function(f) => return Some(f.clone()),
+            };
 
-            if function.is_some() {
-                return function;
-            }
+            node = map.get(expr.get_type())?;
         }
 
-        None
-    }
-}
-
-
-pub struct Signature {
-    pub name: String,
-    pub args: Vec<&'static dyn Type>,
-}
-
-impl PartialEq for Signature {
-    fn eq(&self, other: &Self) -> bool {
-        let args_are_equal = self.args
-            .iter()
-            .zip(other.args.iter())
-            .fold(true, |p, (&a1, &a2)| p && a1.eq(a2));
-
-        self.name == other.name
-            && args_are_equal
-    }
-}
-
-impl Eq for Signature {}
-
-impl Hash for Signature {
-    fn hash<H>(&self, state: &mut H) where H: Hasher {
-        self.name.hash(state);
-        // Arguments can't be part of hash, as types can have different names,
-        // but still be equal (when one of them is "any").
+        match node {
+            Node::Type(_)     => None,
+            Node::Function(f) => Some(f.clone()),
+        }
     }
 }
 
 
 #[derive(Debug)]
+enum Node<T> {
+    Type(HashMap<&'static dyn Type, Node<T>>),
+    Function(T),
+}
+
+impl<T> Node<T> {
+    fn insert(&mut self, args: &[&'static dyn Type], f: T)
+        -> Result<(), Error>
+    {
+        let map = match self {
+            Node::Type(map)   => map,
+            Node::Function(_) => return Err(Error::FunctionAlreadyDefined),
+        };
+
+        let (&t, args) = match args.split_last() {
+            Some(result) => result,
+
+            None => {
+                // We've run out of arguments to look at while unpacking the
+                // already existing nodes on the path to our functions. This
+                // means that a less specific function is already defined.
+                return Err(Error::FunctionAlreadyDefined);
+            }
+        };
+
+        if let Some(node) = map.get_mut(t) {
+            return node.insert(args, f);
+        }
+
+        let mut node = Node::Function(f);
+
+        for &t in args {
+            let mut map = HashMap::new();
+            map.insert(
+                t,
+                node,
+            );
+            node = Node::Type(map);
+        }
+
+        map.insert(
+            t,
+            node,
+        );
+
+        Ok(())
+    }
+}
+
+
+#[derive(Debug, Eq, PartialEq)]
 pub enum Error {
     FunctionAlreadyDefined,
 }
@@ -130,3 +156,149 @@ pub type Builtin =
     fn(&mut dyn Context, Span) -> Result<(), context::Error>;
 pub type Extension<Host> =
     fn(Rc<RefCell<Host>>, &mut dyn Context, Span) -> Result<(), context::Error>;
+
+
+#[cfg(test)]
+mod tests {
+    use crate::data::{
+        expr::{
+            self,
+            Expr as _,
+        },
+        span::Span,
+        stack::Stack,
+        types as t,
+    };
+
+    use super::{
+        Error,
+        Functions,
+    };
+
+
+    #[test]
+    fn it_should_return_none_if_function_wasnt_defined() {
+        let functions = Functions::<()>::new();
+        let stack     = Stack::new();
+
+        let function = functions.get("a", &stack);
+
+        assert_eq!(function, None);
+    }
+
+    #[test]
+    fn it_should_return_functions_that_were_defined() -> Result<(), Error> {
+        let mut functions = Functions::new();
+        let mut stack     = Stack::new();
+
+        functions
+            .define("a", &[&t::Number, &t::Float], 1)?;
+        stack
+            .push(expr::Number::new(0, Span::default()))
+            .push(expr::Float::new(0.0, Span::default()));
+
+        let function = functions.get("a", &stack);
+
+        assert_eq!(function, Some(1));
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_return_the_function_that_matches_the_types_on_the_stack()
+        -> Result<(), Error>
+    {
+        let mut functions = Functions::new();
+        let mut stack     = Stack::new();
+
+        functions
+            .define("a", &[&t::Number, &t::Float ], 1)?
+            .define("a", &[&t::Number, &t::Number], 2)?;
+        stack
+            .push(expr::Number::new(0, Span::default()))
+            .push(expr::Float::new(0.0, Span::default()));
+
+        let function = functions.get("a", &stack);
+
+        assert_eq!(function, Some(1));
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_return_function_without_args_regardless_of_stack()
+        -> Result<(), Error>
+    {
+        let mut functions = Functions::new();
+        let mut stack     = Stack::new();
+
+        functions
+            .define("a", &[], 1)?;
+        stack
+            .push(expr::Number::new(0, Span::default()))
+            .push(expr::Float::new(0.0, Span::default()));
+
+        let function = functions.get("a", &stack);
+
+        assert_eq!(function, Some(1));
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_reject_functions_that_are_already_defined()
+        -> Result<(), Error>
+    {
+        let mut functions = Functions::new();
+
+        let result = functions
+            .define("a", &[&t::Number, &t::Number], 1)?
+            .define("a", &[&t::Number, &t::Number], 2);
+
+        assert_eq!(result.map(|_| ()), Err(Error::FunctionAlreadyDefined));
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_reject_functions_more_specific_than_a_defined_function()
+        -> Result<(), Error>
+    {
+        let mut functions = Functions::new();
+
+        let result = functions
+            .define("a", &[&t::Number, &t::Number], 1)?
+            .define("a", &[&t::Number], 2);
+
+        assert_eq!(result.map(|_| ()), Err(Error::FunctionAlreadyDefined));
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_reject_no_arg_functions_if_name_is_already_taken()
+        -> Result<(), Error>
+    {
+        // This is a special case of the previous test case. Functions with no
+        // arguments are specially handled in the code, so we also need a
+        // special test for them.
+
+        let mut functions = Functions::new();
+
+        let result = functions
+            .define("a", &[&t::Number], 1)?
+            .define("a", &[], 2);
+
+        assert_eq!(result.map(|_| ()), Err(Error::FunctionAlreadyDefined));
+        Ok(())
+    }
+
+    #[test]
+    fn it_should_reject_functions_less_specific_than_a_defined_function()
+        -> Result<(), Error>
+    {
+        let mut functions = Functions::new();
+
+        let result = functions
+            .define("a", &[&t::Number], 1)?
+            .define("a", &[&t::Number, &t::Number], 2);
+
+        assert_eq!(result.map(|_| ()), Err(Error::FunctionAlreadyDefined));
+        Ok(())
+    }
+}
