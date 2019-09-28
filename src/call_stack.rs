13e08a3b0175f37d2aabdc246e1febe10df1fda0
Hanno Braun <hb@hannobraun.de>
2019-09-28 12:09:56 +0200

    Convert indentation to spaces

--- src/call_stack.rs
+++ src/call_stack.rs
@@ -3,23 +3,23 @@ use crate::data::span::Span;
 
 #[derive(Clone, Debug)]
 pub struct CallStack {
-	pub frames: Vec<StackFrame>,
+    pub frames: Vec<StackFrame>,
 }
 
 impl CallStack {
-	pub fn new() -> Self {
-		Self {
-			frames: Vec::new(),
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            frames: Vec::new(),
+        }
+    }
 
-	pub fn operator(&self) -> &StackFrame {
-		self.frames.last()
-			// This shouldn't generally happen, as this is only called by
-			// builtins, and when a builtin is executed, there must be a frame
-			// on the call stack.
-			.expect("Tried to get operator from empty call stack")
-	}
+    pub fn operator(&self) -> &StackFrame {
+        self.frames.last()
+            // This shouldn't generally happen, as this is only called by
+            // builtins, and when a builtin is executed, there must be a frame
+            // on the call stack.
+            .expect("Tried to get operator from empty call stack")
+    }
 }
 
 
