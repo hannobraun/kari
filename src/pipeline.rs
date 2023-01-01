515f43c5a4adffde85b730c6e66dedfe2b111bad
Hanno Braun <hanno@braun-odw.eu>
2023-01-01 12:57:41 +0100

    Remove unused trait bound

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -4,17 +4,12 @@ pub mod tokenizer;
 
 pub use self::{parser::Parser, reader::Reader, tokenizer::Tokenizer};
 
-use std::io;
-
 pub struct Pipeline<R> {
     pub parser: Parser<R>,
 }
 
 impl<R> Pipeline<R> {
-    pub fn new(name: String, stream: R) -> Self
-    where
-        R: io::Read,
-    {
+    pub fn new(name: String, stream: R) -> Self {
         let reader = Reader::new(stream);
         let tokenizer = Tokenizer::new(reader, name);
         let parser = Parser::new(tokenizer);
